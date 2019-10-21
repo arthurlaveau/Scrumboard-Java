4ce6fbf04e6ba1a0d719b5f5809f31bc146b0bf0
arthurlaveau <56834042+arthurlaveau@users.noreply.github.com>
2019-10-21 16:49:11 +0200

    Update ConnexionBdd.java

--- src/main/java/fr/cfai/scrumboard/dao/ConnexionBdd.java
+++ src/main/java/fr/cfai/scrumboard/dao/ConnexionBdd.java
@@ -8,7 +8,7 @@ public class ConnexionBdd {
 
 	 public static Connection getConnection() throws SQLException, ClassNotFoundException {
          Class.forName("com.mysql.jdbc.Driver");
-         Connection connection = DriverManager.getConnection("jdbc:mysql://62.210.119.152:49158/arthur?verifyServerCertificate=false&useSSL=false","arthur","arthur");
+         Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:8000/arthur?verifyServerCertificate=false&useSSL=false","root","root");
          return connection ;
  }
 	 public static void close() throws ClassNotFoundException, SQLException {
